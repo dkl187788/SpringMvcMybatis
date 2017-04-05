5fee69a41323eb76411b323c0d4890fe2e72300d
dkl187788 <13783459623@163.com>
2017-04-06 00:22:57 +0800

    添加实体

---
+++ src/main/java/com/dukl/web/user/dao/GoodsMapper.java
@@ -0,0 +1,11 @@
+package com.dukl.web.user.dao;
+
+import java.util.Map;
+
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+public interface GoodsMapper {
+
+    Map<String,String> obtainMap();
+}
