fd7b29acd1a5f768cc3ab639929bf4f5b227b0d8
dkl187788 <13783459623@163.com>
2017-04-06 00:24:22 +0800

    添加实体

---
+++ src/main/java/com/dukl/web/user/model/GoodsEntity.java
@@ -0,0 +1,27 @@
+package com.dukl.web.user.model;
+
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+public class GoodsEntity {
+
+    private String gId;
+
+    private String gName;
+
+    public String getgId() {
+        return gId;
+    }
+
+    public void setgId(String gId) {
+        this.gId = gId;
+    }
+
+    public String getgName() {
+        return gName;
+    }
+
+    public void setgName(String gName) {
+        this.gName = gName;
+    }
+}
