1f61d51b59b0f8632ecf65b1c16bfeb5688e7973
dkl187788 <13783459623@163.com>
2017-04-09 22:47:07 +0800

    添加生日信息

--- src/main/java/com/dukl/web/user/model/GoodsEntity.java
+++ src/main/java/com/dukl/web/user/model/GoodsEntity.java
@@ -1,5 +1,7 @@
 package com.dukl.web.user.model;
 
+import java.util.Date;
+
 /**
  * Created by Administrator on 2017/4/6.
  */
@@ -8,6 +10,8 @@ public class GoodsEntity {
     private String gId;
 
     private String gName;
+
+    private Date birthdate;
 
     public String getgId() {
         return gId;
@@ -23,5 +27,13 @@ public class GoodsEntity {
 
     public void setgName(String gName) {
         this.gName = gName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
     }
 }
